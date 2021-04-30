cc14d1add8d08501dd2704eeeb539c98bb2264a4
Heriansyah <heriansyah19@gmail.com>
2021-04-30 22:31:59 +0700

    tambah fungsi

--- pythonLatihan.py
+++ pythonLatihan.py
@@ -1,7 +1,16 @@
 print("Selamat datang di kelas python")
-print(*=10)
+#print(=+10)
 
 a = [10,9,8,7,6,5,4,3,2,1]
 
-def x in a:
+for x in a:
   print(x)
+  
+def a(nama,umur):
+    print("nama saya adalah", nama)
+    print("umur saya adalah", umur)
+
+
+a("Ayie",12)
+
+  
